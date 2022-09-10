3a9217fdd4dbf607cdee82deba8808051974b0c3
Ynkcc <55530775+Ynkcc@users.noreply.github.com>
2022-09-10 16:50:17 +0000

    Update array.c

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -134,7 +134,7 @@ static const char * const task_state_array[] = {
 	"S (sleeping)",		/* 0x01 */
 	"D (disk sleep)",	/* 0x02 */
 	"T (stopped)",		/* 0x04 */
-	"t (tracing stop)",	/* 0x08 */
+	"M (modify)",	        /* 0x08 */
 	"X (dead)",		/* 0x10 */
 	"Z (zombie)",		/* 0x20 */
 	"P (parked)",		/* 0x40 */
